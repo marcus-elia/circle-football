defe009505e63d66351dd22a67fb58e20afdebd9
rparker678 <rparker@rparkersystems.com>
2019-10-11 16:49:04 -0400

    Different colors for teams

--- src/FootballPlayer.java
+++ src/FootballPlayer.java
@@ -11,18 +11,20 @@ public class FootballPlayer extends GameObject
 	private double radius; //The radius of the player
 	private static Random rand;	//RNG for movement
 	private boolean collided;
+	private Color color; //The color of the player
 
-    public FootballPlayer(double inputX, double inputY, ID id, double radius)
+    public FootballPlayer(double inputX, double inputY, ID id, double radius, Color color)
     {
         super(inputX, inputY, id);
         this.radius = radius;
         this.rand = new Random();
+        this.color = color;
     }
 
     public boolean isCollided(FootballPlayer otherPlayer)
 	{
-		this.collided = radius > (Math.sqrt(Math.pow(otherPlayer.getX() - this.getX(), 2) + Math.pow(otherPlayer.getY() - this.getY(), 2)));
-		return radius > (Math.sqrt(Math.pow(otherPlayer.getX() - this.getX(), 2) + Math.pow(otherPlayer.getY() - this.getY(), 2)));
+		this.collided = radius > FootballPlayer.distance(this.getX(), this.getY(), otherPlayer.getX(), otherPlayer.getY());
+		return radius > FootballPlayer.distance(this.getX(), this.getY(), otherPlayer.getX(), otherPlayer.getY());
 	}
 
     public void tick()
@@ -49,8 +51,13 @@ public class FootballPlayer extends GameObject
 
     public void render(Graphics2D g2d)
     {
-        g2d.setColor(Color.CYAN);
+        g2d.setColor(this.color);
         Shape circle = new Ellipse2D.Double(this.x, this.y, radius * 2, radius * 2);
         g2d.fill(circle);
     }
+
+    public static double distance(double x1, double y1, double x2, double y2)
+	{
+		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+	}
 }
