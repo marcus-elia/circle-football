2fe88cfba29bb43c3806faef0593452cc9861e7d
marcus-elia <mselia@uvm.edu>
2019-10-11 16:21:36 -0400

    Created a FootballGameManager class that creates two teams of random players

---
+++ src/FootballGameManager.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class FootballGameManager
+{
+    private Game theGame;
+    private double playerRadius;
+    private ArrayList<FootballPlayer> leftTeam;
+    private ArrayList<FootballPlayer> rightTeam;
+
+    public FootballGameManager(Game inputGame, double inputRadius)
+    {
+        this.theGame = inputGame;
+        this.playerRadius = inputRadius;
+        this.leftTeam = createTeam(5);
+        this.rightTeam = createTeam(5);
+    }
+
+    public ArrayList<FootballPlayer> createTeam(int numPlayers)
+    {
+        double randomX, randomY;
+        // The player can spawn anywhere on the field such that it is not overlapping with the border
+        double spawnableWidth = this.theGame.getWidth() - 2*this.playerRadius;
+        double spawnableHeight = this.theGame.getHeight() - 2*this.playerRadius;
+        ArrayList<FootballPlayer> players = new ArrayList<FootballPlayer>();
+        for(int i = 0; i < numPlayers; i++)
+        {
+            randomX = Math.random()*spawnableWidth + this.playerRadius;
+            randomY = Math.random()*spawnableHeight + this.playerRadius;
+            players.add(new FootballPlayer(randomX, randomY, ID.Player, this.playerRadius));
+        }
+
+        return players;
+    }
+}
