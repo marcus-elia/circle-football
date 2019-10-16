b5ba40e7e1d0232ddd00b50e17e79f0a537c5f96
marcus-elia <mselia@uvm.edu>
2019-10-15 20:57:29 -0400

    Made the random bouncer class

---
+++ src/RandomBouncer.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+
+// This class is just a football player that starts moving on a random
+// direction, and then just bounces around. It does not do things with
+// targeting coordinates.
+public class RandomBouncer extends FootballPlayer
+{
+    private double radius;
+    private FootballGameManager manager;
+
+    RandomBouncer(FootballGameManager inputManager,
+                  double inputX, double inputY, ID id, double radius, Color color, double speed)
+    {
+        super(inputManager, inputX, inputY, id, radius, color, speed);
+        this.setAngle(Math.random() * 2*Math.PI);
+    }
+
+    public void tick()
+    {
+        if(this.x < this.radius || this.x > this.manager.getWidth() - this.radius)
+        {
+            this.dx = -this.dx;
+        }
+        if(this.y < this.radius || this.y > this.manager.getHeight() - this.radius)
+        {
+            this.dy = -this.dy;
+        }
+    }
+}
