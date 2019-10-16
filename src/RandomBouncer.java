9dc11a2b947ba68137ba0270eda98bcd04005081
marcus-elia <mselia@uvm.edu>
2019-10-15 21:23:07 -0400

    Added a react to collision function in RandomBouncer. It works by using the angle in = angle out idea of bouncing

--- src/RandomBouncer.java
+++ src/RandomBouncer.java
@@ -13,10 +13,9 @@ public class RandomBouncer extends FootballPlayer
     {
         super(inputManager, inputX, inputY, id, radius, color, speed);
         this.setAngle(Math.random() * 2*Math.PI);
-        this.radius = radius;
-        this.manager = inputManager;
     }
 
+    // This just bounces if it hits a wall.
     public void tick()
     {
         if(this.x < this.radius || this.x > this.manager.getWidth() - this.radius)
@@ -30,5 +29,19 @@ public class RandomBouncer extends FootballPlayer
 
         this.x += this.dx;
         this.y += this.dy;
+    }
+
+    public void reactToCollision(double otherX, double otherY)
+    {
+        double forceAngle = Math.atan2(this.y - otherY, this.x - otherX);
+        double inverseAngle = this.angle + Math.PI;
+        if(inverseAngle > forceAngle)
+        {
+            this.setAngle(forceAngle - (inverseAngle - forceAngle));
+        }
+        else
+        {
+            this.setAngle(forceAngle + (forceAngle - inverseAngle));
+        }
     }
 }
