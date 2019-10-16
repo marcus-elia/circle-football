449cf81683ae2b8670e6375878dcd4cfd82d99b9
marcus-elia <mselia@uvm.edu>
2019-10-15 21:41:28 -0400

    Wrote a trueAngleDifference helper function, and used that to modify the collision reaction

--- src/RandomBouncer.java
+++ src/RandomBouncer.java
@@ -33,13 +33,26 @@ public class RandomBouncer extends FootballPlayer
     {
         double forceAngle = Math.atan2(this.y - otherY, this.x - otherX);
         double inverseAngle = this.angle + Math.PI;
-        if(inverseAngle > forceAngle)
+        double difference = this.trueAngleDifference(forceAngle, inverseAngle);
+        this.setAngle(forceAngle - difference);
+    }
+
+    // Returns the difference between angle1 and angle2 between -Pi and Pi.
+    // For example, trueAngleDifference(Pi/2, 0) = -Pi/2
+    //              trueAngleDifference(0, Pi/2) = Pi/2
+    //              trueAngleDifference(0, 3Pi/2) = -Pi/2
+    //              trueAngleDifference(3Pi/2, 0) = Pi/2
+    public double trueAngleDifference(double angle1, double angle2)
+    {
+        double difference = angle2 - angle1;
+        if(difference > Math.PI)
         {
-            this.setAngle(forceAngle - (inverseAngle - forceAngle));
+            return difference - 2*Math.PI;
         }
-        else
+        if(difference < -Math.PI)
         {
-            this.setAngle(forceAngle + (forceAngle - inverseAngle));
+            return difference + 2*Math.PI;
         }
+        return difference;
     }
 }
