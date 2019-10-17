2a352624bbda82cda8358a0760cf259f431e0009
marcus-elia <mselia@uvm.edu>
2019-10-17 16:58:28 -0400

    Filled in the render function

--- src/Ball.java
+++ src/Ball.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 public class Ball extends GameObject
 {
@@ -16,6 +17,10 @@ public class Ball extends GameObject
 
     public void render(Graphics2D g2d)
     {
-
+        double radius = this.manager.getPlayerRadius();
+        g2d.setColor(Color.YELLOW);
+        Shape circle = new Ellipse2D.Double(this.x - radius, this.y - radius, radius * 2, radius * 2);
+        g2d.setStroke(new BasicStroke(2));
+        g2d.draw(circle);
     }
 }
